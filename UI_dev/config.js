0770e75ec5227fa66077fa0330d8b8774a8ed3f0
kayo5994 <330956999@qq.com>
2016-08-31 20:17:50 +0800

    1. 更新模板引擎和 BrowserSync 的功能说明；2. 根据模板引擎的新功能删除不希望输出的模板；

--- UI_dev/config.js
+++ UI_dev/config.js
@@ -1,18 +1,29 @@
 module.exports = {
-  "commentBase" : "项目相关部分代码，复制后应首先进行这些配置",
   "project" : "Demo",     
   "prefix" : "dm",
   "resultCssFileName" : "main.scss",
   "cleanFileType" : ["../.sass-cache", "../.sass-cache/**/*"],
 
-  "commentPath" : "项目的路径配置，建议尽量使用推荐的路径，若要修改，请保持与 config.rb 中的指向的目录保持一致，但需要注意因为相对位置不同（这里是以 qmui_sass 目录为 Base Path），所以这里的值应该比 config.rb 中的多了一个 ../",
+  "browserSyncMod": "server",
+  "browserSyncPort": 3030,
+  "browserSyncShowLog": false,
+  "browserSyncStartPath": "/web",
+  "browserSyncHost": "",
+  "browserSyncWatchPath":['../../UI_html_result/*.html', '../../public/**/*'],
+  "browserSyncServerRoute": {
+      '/public': '../../public',
+      '/data': '../../data',
+      '/node_modules': '../../node_modules',
+      '/web': '../../UI_html_result'
+  },
+  "browserSyncProxy": "",
+
   "htmlSourcePath" : ["../../UI_html/**/*.html"],
   "imagesSourcePath" : "../project/images", 
   "htmlResultPath" : "../../UI_html_result",
   "imagesResultPath" : "../../public/style/images",
   "independentImagesDirectory" : "/independent", 
 
-  "commentTemplate" : "模板 include 引擎的相关配置",
   "openIncludeFunction" : true,
   "includePrefix" : "@@"
 };   
