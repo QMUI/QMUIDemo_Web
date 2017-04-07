830ede17ef17a3818de56d58a54bbb7d9a804583
kayo5994 <330956999@qq.com>
2017-04-07 17:04:32 +0800

    修改需要变更的文件

--- UI_dev/mergeRule.js
+++ UI_dev/mergeRule.js
@@ -1,4 +1,5 @@
 module.exports = {
-  "../public/js/all.js" : ["../public/js/prettify.js", "../public/js/lang-css.js", "../node_modules/jquery/dist/jquery.min.js", "../public/js/main.js"],
+  "../public/js/all.js" : ["../public/js/highlight.pack.js", "../node_modules/jquery/dist/jquery.min.js", "../public/js/main.js"],
+  "../public/js/tools_release.js" : ["../public/js/qmui_method.js", "../public/js/tools.js"],
   "../public/style/css/main_release.css" : ["../public/style/css/main.css"]
-};   
+};
