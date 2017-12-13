6299710db0304edc9fbb1dbe2691215041cd79d2
jostinsu <jostinsu@qq.com>
2017-12-13 22:23:12 +0800

    feature: add svgSprite task

--- UI_dev/config.js
+++ UI_dev/config.js
@@ -1,32 +1,33 @@
 module.exports = {
-  "project" : "Demo",
-  "prefix" : "dm",
-  "resultCssFileName" : "main.scss",
-  "cleanFileType" : ["../.sass-cache", "../.sass-cache/**/*"],
-  "needsSourceMaps": false,
-  "needsImagesMinAndSync": false,
+    "project": "Demo",
+    "prefix": "dm",
+    "resultCssFileName": "main.scss",
+    "cleanFileType": ["../.sass-cache", "../.sass-cache/**/*"],
+    "needsSourceMaps": false,
+    "needsImagesMinAndSync": false,
 
-  "browserSyncMod": "server",
-  "browserSyncPort": 3030,
-  "browserSyncShowLog": false,
-  "browserSyncStartPath": "/web",
-  "browserSyncHost": "",
-  "browserSyncWatchPath":['../../UI_html_result/*.html', '../../public/**/*'],
-  "browserSyncServerRoute": {
-      '/public': '../../public',
-      '/data': '../../data',
-      '/node_modules': '../../node_modules',
-      '/web': '../../UI_html_result'
-  },
-  "browserSyncProxy": "",
+    "browserSyncMod": "server",
+    "browserSyncPort": 3030,
+    "browserSyncShowLog": false,
+    "browserSyncStartPath": "/web",
+    "browserSyncHost": "",
+    "browserSyncWatchPath": ['../../UI_html_result/*.html', '../../public/**/*'],
+    "browserSyncServerRoute": {
+        '/public': '../../public',
+        '/data': '../../data',
+        '/node_modules': '../../node_modules',
+        '/web': '../../UI_html_result'
+    },
+    "browserSyncProxy": "",
 
-  "htmlSourcePath" : ["../../UI_html/**/*.html"],
-  "imagesSourcePath" : "../project/images",
-  "htmlResultPath" : "../../UI_html_result",
-  "imagesResultPath" : "../../public/style/images",
-  "independentImagesDirectory" : "/independent",
-  "styleResultPath": "../../public/style/css",
+    "htmlSourcePath": ["../../UI_html/**/*.html"],
+    "imagesSourcePath": "../project/images",
+    "htmlResultPath": "../../UI_html_result",
+    "imagesResultPath": "../../public/style/images",
+    "independentImagesDirectory": "/independent",
+    "styleResultPath": "../../public/style/css",
+    "svgResultPath": "../project/svgSprite",
 
-  "openIncludeFunction" : true,
-  "includePrefix" : "@@"
+    "openIncludeFunction": true,
+    "includePrefix": "@@"
 };
