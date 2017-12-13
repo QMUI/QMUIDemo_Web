0cbc6ab3b38e21a055ad1538dce00b803bcc3e1c
kayo5994 <330956999@qq.com>
2017-12-13 14:19:00 +0800

    update: set 4 spaces indentation for JavaScript files and remove gulp error monitoring of sass task

--- public/js/main.js
+++ public/js/main.js
@@ -23,35 +23,35 @@
 // }
 
 // 增加一个 class
-var addClass = function(e, c) {
-  var t = ' ' + e.className + ' ',
-      cs = c.split(' ');
+var addClass = function (e, c) {
+    var t = ' ' + e.className + ' ',
+        cs = c.split(' ');
 
-  for( var i = 0; i < cs.length; i++ ){
+    for (var i = 0; i < cs.length; i++) {
 
-    var current = cs[i];
+        var current = cs[i];
 
-    t.indexOf(' ' + current + ' ') === -1 && ( t += ' ' + current );
+        t.indexOf(' ' + current + ' ') === -1 && (t += ' ' + current);
 
-    t = t.replace('  ', ' '); // 去掉连续两个空格
-  }
+        t = t.replace('  ', ' '); // 去掉连续两个空格
+    }
 
-  e.className = t.replace(/(^\s*)|(\s*$)/g, ''); // 去掉首尾空白
+    e.className = t.replace(/(^\s*)|(\s*$)/g, ''); // 去掉首尾空白
 }
 
 // 移除一个 class
-var removeClass = function(e, c) {
-  var t = ' ' + e.className + ' ',
-      cs = c.split(' ');
+var removeClass = function (e, c) {
+    var t = ' ' + e.className + ' ',
+        cs = c.split(' ');
 
-  for( var i = 0; i < cs.length; i++ ){
+    for (var i = 0; i < cs.length; i++) {
 
-    var current = cs[i];
+        var current = cs[i];
 
-    t.indexOf(' ' + current + ' ') >= 0 && ( t = t.replace(' ' + current + ' ', ' ') );
-  }
+        t.indexOf(' ' + current + ' ') >= 0 && (t = t.replace(' ' + current + ' ', ' '));
+    }
 
-  e.className = t.replace(/(^\s*)|(\s*$)/g, ''); // 去掉首尾空白
+    e.className = t.replace(/(^\s*)|(\s*$)/g, ''); // 去掉首尾空白
 }
 
 
@@ -62,103 +62,103 @@ var showMenuBtn = document.getElementById('showMenuBtn'),
     closeMenuBtn = document.getElementById('closeMenuBtn'),
     header = document.getElementById('head');
 
-showMenuBtn.onclick = function() {
-  addClass(header, 'frame_head_ShowNav');
+showMenuBtn.onclick = function () {
+    addClass(header, 'frame_head_ShowNav');
 }
-closeMenuBtn.onclick = function() {
-  removeClass(header, 'frame_head_ShowNav');
+closeMenuBtn.onclick = function () {
+    removeClass(header, 'frame_head_ShowNav');
 }
 
 // 调用代码高亮方法
 var tagsToReplace = {
-  '<': '&lt;',
-  '>': '&gt;'
+    '<': '&lt;',
+    '>': '&gt;'
 };
-$('pre code').each(function(i, block) {
-  var result = $(block).html();
-  result = result.replace(/[&<>]/g, function(tag) {
-    return tagsToReplace[tag] || tag;
-  });
-  $(block).html(result);
-  hljs.highlightBlock(block);
+$('pre code').each(function (i, block) {
+    var result = $(block).html();
+    result = result.replace(/[&<>]/g, function (tag) {
+        return tagsToReplace[tag] || tag;
+    });
+    $(block).html(result);
+    hljs.highlightBlock(block);
 });
 
 // 公共组件遮罩层效果展示
 var showMask = document.getElementById('qui_showMask'),
     maskWrap = document.getElementById('qui_maskWrap');
-if(showMask && maskWrap) {
-  showMask.onclick = function(){
-    maskWrap.style.display = 'block';
-  }
-  maskWrap.onclick = function(){
-    maskWrap.style.display = 'none';
-  }
+if (showMask && maskWrap) {
+    showMask.onclick = function () {
+        maskWrap.style.display = 'block';
+    }
+    maskWrap.onclick = function () {
+        maskWrap.style.display = 'none';
+    }
 }
 
 /* eslint-disable */
 // 第三方框架调用，忽略检测
 // 微信接口
 var currentUrlWithoutHash = window.location.href.replace(window.location.hash, '');
-$.getJSON('http://qmuiteam.com/wxapi/signature.php?url=' + encodeURIComponent(currentUrlWithoutHash)).done(function(result) {
-  wx.config({
-    debug: false,
-    appId: result.appId,
-    timestamp: result.timestamp,
-    nonceStr: result.nonceStr,
-    signature: result.signature,
-    jsApiList: [
-    'onMenuShareTimeline',
-    'onMenuShareAppMessage'
-    ]
-  });
+$.getJSON('http://qmuiteam.com/wxapi/signature.php?url=' + encodeURIComponent(currentUrlWithoutHash)).done(function (result) {
+    wx.config({
+        debug: false,
+        appId: result.appId,
+        timestamp: result.timestamp,
+        nonceStr: result.nonceStr,
+        signature: result.signature,
+        jsApiList: [
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage'
+        ]
+    });
 });
 
-wx.ready(function() {
-  var currentUrl = window.location.href,
-      isIndex = currentUrl.indexOf('index') != -1 || currentUrl.indexOf('html') == -1,
-      config = {
-        desc: '一个旨在提高 UI 开发效率，快速产生项目 UI 的前端框架', // 分享描述
-        link: currentUrl, // 分享链接
-        imgUrl: 'http://qmuiteam.com/web/public/style/images/independent/share/ShareLogo.png', // 分享图标
-        success: function() {
-          console.log('分享成功');
+wx.ready(function () {
+    var currentUrl = window.location.href,
+        isIndex = currentUrl.indexOf('index') != -1 || currentUrl.indexOf('html') == -1,
+        config = {
+            desc: '一个旨在提高 UI 开发效率，快速产生项目 UI 的前端框架', // 分享描述
+            link: currentUrl, // 分享链接
+            imgUrl: 'http://qmuiteam.com/web/public/style/images/independent/share/ShareLogo.png', // 分享图标
+            success: function () {
+                console.log('分享成功');
+            },
+            cancel: function () {
+                console.log('分享失败');
+            }
         },
-        cancel: function() {
-          console.log('分享失败');
-        }
-      },
-      timelineConfig = $.extend(true, {}, config),
-      shareAppMessageConfig = $.extend(true, {}, config);
+        timelineConfig = $.extend(true, {}, config),
+        shareAppMessageConfig = $.extend(true, {}, config);
 
-  timelineConfig.title = document.title;
-  shareAppMessageConfig.title = isIndex ? 'QMUI Web' : document.title;
-  wx.onMenuShareTimeline(timelineConfig);
-  wx.onMenuShareAppMessage(shareAppMessageConfig);
+    timelineConfig.title = document.title;
+    shareAppMessageConfig.title = isIndex ? 'QMUI Web' : document.title;
+    wx.onMenuShareTimeline(timelineConfig);
+    wx.onMenuShareAppMessage(shareAppMessageConfig);
 });
 
 // Eruda
 if (window.localStorage) {
-	(function () {
-		var src = '../node_modules/eruda/eruda.min.js';
-		if (!(/eruda=true/).test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
-		document.write('<scr' +
-        'ipt src="' +
-        src +
-        '"></scr'+
-        'ipt>');
-		document.write('<scr' +
-        'ipt>eruda.init();</scr' +
-        'ipt>');
-	})();
+    (function () {
+        var src = '../node_modules/eruda/eruda.min.js';
+        if (!(/eruda=true/).test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
+        document.write('<scr' +
+            'ipt src="' +
+            src +
+            '"></scr' +
+            'ipt>');
+        document.write('<scr' +
+            'ipt>eruda.init();</scr' +
+            'ipt>');
+    })();
 }
 
 // 统计
 var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement('script');
-  hm.src = '//hm.baidu.com/hm.js?b0246dc2e24129a9a91fc47e6f5a9d9f';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(hm, s);
+(function () {
+    var hm = document.createElement('script');
+    hm.src = '//hm.baidu.com/hm.js?b0246dc2e24129a9a91fc47e6f5a9d9f';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(hm, s);
 })();
 /* eslint-enable */
 
@@ -167,81 +167,81 @@ var _hmt = _hmt || [];
 var isNavInClick = false;
 
 // 确认侧边栏上的目录是否已经进入 Active 态
-var frameSidebarItemIsInActive = function(target) {
-  var offsetTop = $(target).offset().top,
-    height = $(target).outerHeight();
-  if (offsetTop > -height + 46) { // 46为顶部bar的高度
+var frameSidebarItemIsInActive = function (target) {
+    var offsetTop = $(target).offset().top,
+        height = $(target).outerHeight();
+    if (offsetTop > -height + 46) { // 46为顶部bar的高度
 
-    return true;
-  }
+        return true;
+    }
 
-  return false;
+    return false;
 }
 
 // 触发侧边栏进入 Active 态
-var makeFrameSidebarActive = function(columnIndex, itemIndex) {
-  var group = $('.js_sidebar_group').removeClass('frame_sidebar_nav_item_Open').eq(columnIndex).addClass('frame_sidebar_nav_item_Open');
-  $('.js_sidebar_item').removeClass('frame_sidebar_nav_item_Active');
-  if (group.size() > 0) {
-    group.find('.js_sidebar_item').eq(itemIndex).addClass('frame_sidebar_nav_item_Active');
-  }
+var makeFrameSidebarActive = function (columnIndex, itemIndex) {
+    var group = $('.js_sidebar_group').removeClass('frame_sidebar_nav_item_Open').eq(columnIndex).addClass('frame_sidebar_nav_item_Open');
+    $('.js_sidebar_item').removeClass('frame_sidebar_nav_item_Active');
+    if (group.size() > 0) {
+        group.find('.js_sidebar_item').eq(itemIndex).addClass('frame_sidebar_nav_item_Active');
+    }
 }
 
 // 确认侧边栏是否被点击
-var confirmNavIsInClick = function() {
-  var timerAtClickNav = null; // 由于导航栏点击的时候,也会触发页面滚动条滚动，这个标志位是为了当导航栏点击之后的 100ms 内不触发页面滚动条事件
-  isNavInClick = true;
-  clearTimeout(timerAtClickNav);
-  timerAtClickNav = setTimeout(function() {
-    isNavInClick = false;
-  }, 100);
+var confirmNavIsInClick = function () {
+    var timerAtClickNav = null; // 由于导航栏点击的时候,也会触发页面滚动条滚动，这个标志位是为了当导航栏点击之后的 100ms 内不触发页面滚动条事件
+    isNavInClick = true;
+    clearTimeout(timerAtClickNav);
+    timerAtClickNav = setTimeout(function () {
+        isNavInClick = false;
+    }, 100);
 }
 
 makeFrameSidebarActive(0, 0); // 初始化侧边栏
 
 // 为侧边栏绑定滚动事件
-$('.frame_cnt').on('scroll', function() {
-  if(!isNavInClick){  // 当用户进行点击时,不响应滚动事件
-    console.log('come in');
-     var frameCntHeight = $(this).outerHeight(true), // 可见高度
-       frameCntContentHeight = $(this).get(0).scrollHeight, // 内容高度
-       frameCntScrollTop = $(this).scrollTop(), // 滚动高度
-       columnIndex = 0,
-       itemIndex = 0;
-     if (frameCntHeight + frameCntScrollTop >= frameCntContentHeight) { // 到达页面底部的特殊处理
-       columnIndex = $('.dm_column').size() - 1;
-       itemIndex = $('.dm_column').last().find('.dm_column_item').size() - 1;
-     } else {  // 页面滚动的处理
-       $('.dm_column').each(function(i) { // eslint-disable-line
-         if (frameSidebarItemIsInActive(this)) {
-           columnIndex = i;
-           $(this).children('.dm_column_item').each(function(j) { // eslint-disable-line
-             if (frameSidebarItemIsInActive(this)) {
-               itemIndex = j;
+$('.frame_cnt').on('scroll', function () {
+    if (!isNavInClick) {  // 当用户进行点击时,不响应滚动事件
+        console.log('come in');
+        var frameCntHeight = $(this).outerHeight(true), // 可见高度
+            frameCntContentHeight = $(this).get(0).scrollHeight, // 内容高度
+            frameCntScrollTop = $(this).scrollTop(), // 滚动高度
+            columnIndex = 0,
+            itemIndex = 0;
+        if (frameCntHeight + frameCntScrollTop >= frameCntContentHeight) { // 到达页面底部的特殊处理
+            columnIndex = $('.dm_column').size() - 1;
+            itemIndex = $('.dm_column').last().find('.dm_column_item').size() - 1;
+        } else {  // 页面滚动的处理
+            $('.dm_column').each(function (i) { // eslint-disable-line
+                if (frameSidebarItemIsInActive(this)) {
+                    columnIndex = i;
+                    $(this).children('.dm_column_item').each(function (j) { // eslint-disable-line
+                        if (frameSidebarItemIsInActive(this)) {
+                            itemIndex = j;
 
-               return false;
-             }
-           });
+                            return false;
+                        }
+                    });
 
-           return false;
-         }
-       });
-     }
-     makeFrameSidebarActive(columnIndex, itemIndex);
-  }
+                    return false;
+                }
+            });
+        }
+        makeFrameSidebarActive(columnIndex, itemIndex);
+    }
 });
 
 
 // 为侧边栏一级目录绑定点击事件
-$('.frame_sidebar_nav').delegate('.js_sidebar_group', 'click', function() {
-  confirmNavIsInClick();
-  makeFrameSidebarActive($('.js_sidebar_group').index(this), 0);
+$('.frame_sidebar_nav').delegate('.js_sidebar_group', 'click', function () {
+    confirmNavIsInClick();
+    makeFrameSidebarActive($('.js_sidebar_group').index(this), 0);
 });
 
 // 为侧边栏二级目录绑定点击事件
-$('.frame_sidebar_nav').delegate('.js_sidebar_item', 'click', function(event) {
-  confirmNavIsInClick();
-  console.log($(this).html());
-  $(this).addClass('frame_sidebar_nav_item_Active').siblings('.js_sidebar_item').removeClass('frame_sidebar_nav_item_Active');
-  event.stopPropagation();
+$('.frame_sidebar_nav').delegate('.js_sidebar_item', 'click', function (event) {
+    confirmNavIsInClick();
+    console.log($(this).html());
+    $(this).addClass('frame_sidebar_nav_item_Active').siblings('.js_sidebar_item').removeClass('frame_sidebar_nav_item_Active');
+    event.stopPropagation();
 });
--- public/js/tools.js
+++ public/js/tools.js
@@ -4,132 +4,132 @@
  * @hughkli, Kayo 动态生成工具方法tools.html的DOM内容
  */
 // 利用 SASS 方法的 item 数据拼接方法声明
-var makeCompleteMethodWithItem = function(item, content) {
-  var result = '',
-      itemType = null;
+var makeCompleteMethodWithItem = function (item, content) {
+    var result = '',
+        itemType = null;
 
-  if (item.context.type === 'placeholder') {
-    itemType = '%';
-  } else {
-    itemType = item.context.type + ' ';
-    result = '@';
-  }
-
-  result = result + itemType + item.context.name;
-  if (item.parameter) {
-    result += '(';
-
-    var paraList = item.parameter;
-    for (var paraIndex = 0; paraIndex < paraList.length; paraIndex++) {
-      var paraItem = paraList[paraIndex];
-      if (paraIndex !== 0) {
-        result += ', $';
-      } else {
-        result += '$';
-      }
-      result += paraItem.name;
-      if (paraItem.defaultValue) {
-        result = result + ': ' + paraItem.defaultValue;
-      }
+    if (item.context.type === 'placeholder') {
+        itemType = '%';
+    } else {
+        itemType = item.context.type + ' ';
+        result = '@';
     }
-    result += ')';
-  }
 
-	if (content) {
-		result += ' {\n' + content + '}';
-	} else {
-		result += ' { ... }';
-	}
+    result = result + itemType + item.context.name;
+    if (item.parameter) {
+        result += '(';
 
-  return result;
+        var paraList = item.parameter;
+        for (var paraIndex = 0; paraIndex < paraList.length; paraIndex++) {
+            var paraItem = paraList[paraIndex];
+            if (paraIndex !== 0) {
+                result += ', $';
+            } else {
+                result += '$';
+            }
+            result += paraItem.name;
+            if (paraItem.defaultValue) {
+                result = result + ': ' + paraItem.defaultValue;
+            }
+        }
+        result += ')';
+    }
+
+    if (content) {
+        result += ' {\n' + content + '}';
+    } else {
+        result += ' { ... }';
+    }
+
+    return result;
 };
 
 var siderHtml = [],     // 侧栏
     mainHtml = [];      // 主体
 
 for (var i = 0, llength = comments.length; i < llength; i++) {
-	var tool = comments[i],
-      title = tool[0].group, // 使用第一个方法的分组作为该组标题
-      id = 'qui_' + tool[0].context.name + 'Parent'; // 使用第一个方法的名称加 qui 前缀作为该组 id
-	siderHtml.push('<li class="frame_sidebar_nav_item js_sidebar_group">');
-	siderHtml.push('<a class="frame_sidebar_nav_link" href="#' + id + '">' + title + '</a>');
+    var tool = comments[i],
+        title = tool[0].group, // 使用第一个方法的分组作为该组标题
+        id = 'qui_' + tool[0].context.name + 'Parent'; // 使用第一个方法的名称加 qui 前缀作为该组 id
+    siderHtml.push('<li class="frame_sidebar_nav_item js_sidebar_group">');
+    siderHtml.push('<a class="frame_sidebar_nav_link" href="#' + id + '">' + title + '</a>');
 
-	siderHtml.push('<ul class="frame_sidebar_nav frame_sidebar_nav_Children">');
-  for (var sidebarItemIndex = 0; sidebarItemIndex < tool.length; sidebarItemIndex++) {
-    var sidebarItem = tool[sidebarItemIndex],
-				sidebarItemId = 'qui_' + sidebarItem.context.name;
-		siderHtml.push('  <li class="frame_sidebar_nav_item js_sidebar_item">');
-		siderHtml.push('    <a class="frame_sidebar_nav_link" href="#' + sidebarItemId + '">' + sidebarItem.context.name + '</a>');
-		siderHtml.push('  </li>');
-	}
-	siderHtml.push('</ul>');
-
-	siderHtml.push('</li>');
-
-
-  mainHtml.push('<div class="dm_column">');
-  mainHtml.push('  <h2 class="dm_column_title" id="' + id + '">' + title + '</h2>');
-
-  for (var contentItemIndex = 0; contentItemIndex < tool.length; contentItemIndex++) {
-    var contentItem = tool[contentItemIndex],
-				contentItemId = 'qui_' + contentItem.context.name;
-
-    mainHtml.push('<div class="dm_column_item tool_stage_item" data-showDetail=false data-groupIndex="' + i + '" data-itemIndex="' + contentItemIndex + '">');
-    mainHtml.push('<h3 class="dm_column_item_title" id="' + contentItemId + '">' + contentItem.context.name + '</h3>');
-    mainHtml.push('<p class="tool_stage_item_desc">' + contentItem.description + '</p>');
-
-    if (contentItem.throw) {
-      mainHtml.push('<p class="tool_stage_item_desc"><strong>注意：</strong>' + contentItem.throw + '</p>');
+    siderHtml.push('<ul class="frame_sidebar_nav frame_sidebar_nav_Children">');
+    for (var sidebarItemIndex = 0; sidebarItemIndex < tool.length; sidebarItemIndex++) {
+        var sidebarItem = tool[sidebarItemIndex],
+            sidebarItemId = 'qui_' + sidebarItem.context.name;
+        siderHtml.push('  <li class="frame_sidebar_nav_item js_sidebar_item">');
+        siderHtml.push('    <a class="frame_sidebar_nav_link" href="#' + sidebarItemId + '">' + sidebarItem.context.name + '</a>');
+        siderHtml.push('  </li>');
     }
+    siderHtml.push('</ul>');
 
-    // 参数列表
-    if (contentItem.deprecated) {
-      mainHtml.push('<p class="tool_stage_item_desc"><strong>不推荐使用：</strong>' + contentItem.deprecated + '</p>');
-    }
+    siderHtml.push('</li>');
 
-    mainHtml.push('<div class="dm_column_item_info dm_column_item_info_Single">');
-    mainHtml.push('  <div class="dm_column_item_info_code"><pre><code class="scss">' + makeCompleteMethodWithItem(contentItem) + '</code></pre></div>');
-    mainHtml.push('</div>');
 
-    // 参数列表
-    if (contentItem.parameter) {
-      mainHtml.push('<div class="tool_stage_para">');
-      mainHtml.push('  <div class="tool_stage_para_title">参数列表</div>');
-      mainHtml.push('  <div class="tool_stage_para_cnt">');
+    mainHtml.push('<div class="dm_column">');
+    mainHtml.push('  <h2 class="dm_column_title" id="' + id + '">' + title + '</h2>');
 
-      var paraList = contentItem.parameter;
-      for (var paraIndex = 0; paraIndex < paraList.length; paraIndex++) {
-        var paraItem = paraList[paraIndex];
-        mainHtml.push('<div class="tool_stage_para_item">');
-        mainHtml.push('<div class="tool_stage_item_para_data"><strong>$' + paraItem.name + '</strong><span class="tool_stage_paraType">' + paraItem.type + '</span>');
-        if (paraItem.defaultValue) {
-          mainHtml.push('<span class="tool_stage_paraDefaultValue">Default: ' + paraItem.defaultValue + '</span>');
+    for (var contentItemIndex = 0; contentItemIndex < tool.length; contentItemIndex++) {
+        var contentItem = tool[contentItemIndex],
+            contentItemId = 'qui_' + contentItem.context.name;
+
+        mainHtml.push('<div class="dm_column_item tool_stage_item" data-showDetail=false data-groupIndex="' + i + '" data-itemIndex="' + contentItemIndex + '">');
+        mainHtml.push('<h3 class="dm_column_item_title" id="' + contentItemId + '">' + contentItem.context.name + '</h3>');
+        mainHtml.push('<p class="tool_stage_item_desc">' + contentItem.description + '</p>');
+
+        if (contentItem.throw) {
+            mainHtml.push('<p class="tool_stage_item_desc"><strong>注意：</strong>' + contentItem.throw + '</p>');
+        }
+
+        // 参数列表
+        if (contentItem.deprecated) {
+            mainHtml.push('<p class="tool_stage_item_desc"><strong>不推荐使用：</strong>' + contentItem.deprecated + '</p>');
+        }
+
+        mainHtml.push('<div class="dm_column_item_info dm_column_item_info_Single">');
+        mainHtml.push('  <div class="dm_column_item_info_code"><pre><code class="scss">' + makeCompleteMethodWithItem(contentItem) + '</code></pre></div>');
+        mainHtml.push('</div>');
+
+        // 参数列表
+        if (contentItem.parameter) {
+            mainHtml.push('<div class="tool_stage_para">');
+            mainHtml.push('  <div class="tool_stage_para_title">参数列表</div>');
+            mainHtml.push('  <div class="tool_stage_para_cnt">');
+
+            var paraList = contentItem.parameter;
+            for (var paraIndex = 0; paraIndex < paraList.length; paraIndex++) {
+                var paraItem = paraList[paraIndex];
+                mainHtml.push('<div class="tool_stage_para_item">');
+                mainHtml.push('<div class="tool_stage_item_para_data"><strong>$' + paraItem.name + '</strong><span class="tool_stage_paraType">' + paraItem.type + '</span>');
+                if (paraItem.defaultValue) {
+                    mainHtml.push('<span class="tool_stage_paraDefaultValue">Default: ' + paraItem.defaultValue + '</span>');
+                }
+                mainHtml.push('</div>');
+                mainHtml.push('<div class="tool_stage_item_para_data">' + paraItem.description + '</div>');
+                mainHtml.push('</div>');
+            }
+            mainHtml.push('  </div>');
+            mainHtml.push('</div>');
+        }
+
+        // 例子
+        if (contentItem.example) {
+            var exampleList = contentItem.example;
+            mainHtml.push('<div class="tool_stage_example">');
+            mainHtml.push('  <div class="tool_stage_example_title">示例</div>');
+            for (var exampleIndex = 0; exampleIndex < exampleList.length; exampleIndex++) {
+                var exampleItem = exampleList[exampleIndex];
+                mainHtml.push('  <div class="dm_column_item_info dm_column_item_info_Single">');
+                mainHtml.push('    <div class="dm_column_item_info_code"><pre><code class="bash">' + exampleItem.code + '</code></pre></div>');
+            }
+            mainHtml.push('  </div>');
+            mainHtml.push('</div>');
         }
         mainHtml.push('</div>');
-        mainHtml.push('<div class="tool_stage_item_para_data">' + paraItem.description + '</div>');
-        mainHtml.push('</div>');
-      }
-      mainHtml.push('  </div>');
-      mainHtml.push('</div>');
     }
 
-    // 例子
-    if (contentItem.example) {
-      var exampleList = contentItem.example;
-      mainHtml.push('<div class="tool_stage_example">');
-      mainHtml.push('  <div class="tool_stage_example_title">示例</div>');
-      for (var exampleIndex = 0; exampleIndex < exampleList.length; exampleIndex++) {
-        var exampleItem = exampleList[exampleIndex];
-        mainHtml.push('  <div class="dm_column_item_info dm_column_item_info_Single">');
-        mainHtml.push('    <div class="dm_column_item_info_code"><pre><code class="bash">' + exampleItem.code + '</code></pre></div>');
-      }
-      mainHtml.push('  </div>');
-      mainHtml.push('</div>');
-    }
     mainHtml.push('</div>');
-  }
-
-	mainHtml.push('</div>');
 
 }
 
@@ -137,26 +137,26 @@ document.getElementById('toolSidebarNav').innerHTML = document.getElementById('t
 document.getElementById('toolMain').innerHTML = document.getElementById('toolMain').innerHTML + mainHtml.join('');
 
 // 展开/收起方法实现详情
-$('.tool_stage .dm_column_item_info_code').on('click', function() {
-	var groupIndex = $(this).parent().parent().data('groupindex'),
-			itemIndex = $(this).parent().parent().data('itemindex');
+$('.tool_stage .dm_column_item_info_code').on('click', function () {
+    var groupIndex = $(this).parent().parent().data('groupindex'),
+        itemIndex = $(this).parent().parent().data('itemindex');
 
-  if (typeof groupIndex !== 'undefined' && typeof itemIndex !== 'undefined') {
-    // 序号允许为0，因此不能直接判断 groupIndex && itemIndex
-    var item = comments[groupIndex][itemIndex],
-        codeContent = null;
+    if (typeof groupIndex !== 'undefined' && typeof itemIndex !== 'undefined') {
+        // 序号允许为0，因此不能直接判断 groupIndex && itemIndex
+        var item = comments[groupIndex][itemIndex],
+            codeContent = null;
 
-    if (!$(this).data('showDetail')) {
-      var itemCode = comments[groupIndex][itemIndex].context.code.replace(/^\n/, '');
-      codeContent = hljs.highlight('scss', makeCompleteMethodWithItem(item, itemCode));
-      $(this).html('<pre><code class="scss hljs">' + codeContent.value + '</code></pre>');
-      $(this).data('showDetail', true);
-    } else {
-      codeContent = hljs.highlight('scss', makeCompleteMethodWithItem(item));
-      $(this).html('<pre><code class="scss hljs">' + codeContent.value + '</code></pre>');
-      $(this).data('showDetail', false);
+        if (!$(this).data('showDetail')) {
+            var itemCode = comments[groupIndex][itemIndex].context.code.replace(/^\n/, '');
+            codeContent = hljs.highlight('scss', makeCompleteMethodWithItem(item, itemCode));
+            $(this).html('<pre><code class="scss hljs">' + codeContent.value + '</code></pre>');
+            $(this).data('showDetail', true);
+        } else {
+            codeContent = hljs.highlight('scss', makeCompleteMethodWithItem(item));
+            $(this).html('<pre><code class="scss hljs">' + codeContent.value + '</code></pre>');
+            $(this).data('showDetail', false);
+        }
     }
-  }
 });
 
 // 默认调用一次代码高亮方法
