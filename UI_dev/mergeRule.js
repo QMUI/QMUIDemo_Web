b33cbdee0c18610d23d868932faeda8c6a91a944
kayo5994 <330956999@qq.com>
2017-12-14 21:34:39 +0800

    feature: adaptation of gulp4 and some optimization based on eslint rules

--- UI_dev/mergeRule.js
+++ UI_dev/mergeRule.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "../public/js/all.js" : ["../public/js/highlight.pack.js", "../node_modules/jquery/dist/jquery.min.js", "../public/js/main.js"],
-  "../public/js/tools_release.js" : ["../public/js/qmui_method.js", "../public/js/tools.js"],
-  "../public/style/css/main_release.css" : ["../public/style/css/main.css"]
+    '../public/js/all.js': ['../public/js/highlight.pack.js', '../node_modules/jquery/dist/jquery.min.js', '../public/js/main.js'],
+    '../public/js/tools_release.js': ['../data/qmui_method.js', '../public/js/tools.js'],
+    '../public/style/css/main_release.css': ['../public/style/css/main.css']
 };
